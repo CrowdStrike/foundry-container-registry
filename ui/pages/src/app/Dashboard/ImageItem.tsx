bed1f0f5b1723950bc3047c6213775702279a1f3
Carlos Matos <carlos.matos@crowdstrike.com>
2025-01-13 16:38:10 -0500

    refactored page to be simpler

--- ui/pages/src/app/Dashboard/ImageItem.tsx
+++ ui/pages/src/app/Dashboard/ImageItem.tsx
@@ -1,6 +1,5 @@
 import Image from "@app/types/Image";
 import {
-  Button,
   DataListCell,
   DataListContent,
   DataListItem,
@@ -12,15 +11,11 @@ import {
   DescriptionListGroup,
   DescriptionListTerm,
   Label,
-  Modal,
-  ModalBody,
   Pagination,
   PaginationVariant,
-  Stack,
-  StackItem,
   Title,
 } from "@patternfly/react-core";
-import { CubeIcon, EyeIcon, EyeSlashIcon } from "@patternfly/react-icons";
+import { CubeIcon } from "@patternfly/react-icons";
 import { Table, Tbody, Td, Th, Thead, Tr } from "@patternfly/react-table";
 import React from "react";
 
@@ -30,25 +25,10 @@ interface ImageItemProps {
 
 export function ImageItem({ image }: ImageItemProps) {
   const [isExpanded, setIsExpanded] = React.useState(false);
-  const [isModalOpen, setIsModalOpen] = React.useState(false);
-  const [selectedTag, setSelectedTag] = React.useState<string>("");
-  const [selectedDigest, setSelectedDigest] = React.useState<string>("");
-  const [showPassword, setShowPassword] = React.useState(false);
-  const [showPullToken, setShowPullToken] = React.useState(false);
 
   // Pagination state
   const [page, setPage] = React.useState(1);
   const [perPage, setPerPage] = React.useState(10);
-
-  const handleModalToggle = (_event: KeyboardEvent | React.MouseEvent) => {
-    setIsModalOpen(!isModalOpen);
-  };
-
-  const handleDownloadClick = (tagName: string, digest: string) => {
-    setSelectedTag(tagName);
-    setSelectedDigest(digest);
-    setIsModalOpen(true);
-  };
 
   // Pagination handlers
   const onSetPage = (
@@ -112,9 +92,9 @@ export function ImageItem({ image }: ImageItemProps) {
                   </DescriptionListDescription>
                 </DescriptionListGroup>
                 <DescriptionListGroup>
-                  <DescriptionListTerm>Latest digest</DescriptionListTerm>
+                  <DescriptionListTerm>Image Path</DescriptionListTerm>
                   <DescriptionListDescription>
-                    <code>{image.digest}</code>
+                    <code>{image.repository}</code>
                   </DescriptionListDescription>
                 </DescriptionListGroup>
               </DescriptionList>
@@ -127,57 +107,6 @@ export function ImageItem({ image }: ImageItemProps) {
         isHidden={!isExpanded}
         className="image-details"
       >
-        {/* <Content component={ContentVariants.h3}>Registry Credentials</Content> */}
-        <DescriptionList isHorizontal isCompact>
-          <DescriptionListGroup>
-            <DescriptionListTerm>Registry</DescriptionListTerm>
-            <DescriptionListDescription>
-              <code>{image.registry}</code>
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-
-          <DescriptionListGroup>
-            <DescriptionListTerm>User</DescriptionListTerm>
-            <DescriptionListDescription>
-              <code>{image.login}</code>
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-
-          <DescriptionListGroup>
-            <DescriptionListTerm>Password</DescriptionListTerm>
-            <DescriptionListDescription>
-              <code>{showPassword ? image.password : "••••••••••"}</code>
-              <Button
-                variant="control"
-                onClick={() => setShowPassword(!showPassword)}
-                aria-label={showPassword ? "Hide password" : "Show password"}
-              >
-                {showPassword ? <EyeSlashIcon /> : <EyeIcon />}
-              </Button>
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-
-          <DescriptionListGroup>
-            <DescriptionListTerm>Pull Token</DescriptionListTerm>
-            <DescriptionListDescription>
-              <code>
-                {showPullToken ? image.dockerAuthConfig : "••••••••••"}
-              </code>
-              <Button
-                variant="control"
-                onClick={() => setShowPullToken(!showPullToken)}
-                aria-label={
-                  showPullToken ? "Hide pull token" : "Show pull token"
-                }
-              >
-                {showPullToken ? <EyeSlashIcon /> : <EyeIcon />}
-              </Button>
-            </DescriptionListDescription>
-          </DescriptionListGroup>
-        </DescriptionList>
-        {/* <div className="pf-v5-u-my-md">
-          <Divider />
-        </div> */}
         <Pagination
           itemCount={image.tags.length}
           perPage={perPage}
@@ -211,43 +140,13 @@ export function ImageItem({ image }: ImageItemProps) {
                   ))}
                 </Td>
                 <Td>
-                  <code>{t.digest.substring(0, 19)}</code>
+                  <code>{t.digest}</code>
                 </Td>
               </Tr>
             ))}
           </Tbody>
         </Table>
       </DataListContent>
-      {/* Need to Keep Modal out of the Table mapping to prevent multiple modals being created per tag */}
-      <Modal
-        isOpen={isModalOpen}
-        onClose={handleModalToggle}
-        variant="medium"
-        appendTo={document.body}
-        aria-label="Pull command modal"
-        style={{ minWidth: "fit-content", maxWidth: "100ch" }}
-      >
-        <ModalBody id="modal-box-body-custom-focus">
-          <Stack hasGutter>
-            <h3 style={{ fontWeight: "bold" }}>By Tag</h3>
-            <StackItem>
-              {/* <Title headingLevel="h4">By Tag</Title> */}
-              <code>{`docker pull ${image.repository}:${selectedTag}`}</code>
-            </StackItem>
-            <StackItem>
-              <code>{`podman pull ${image.repository}:${selectedTag}`}</code>
-            </StackItem>
-
-            <h3 style={{ fontWeight: "bold" }}>By Digest</h3>
-            <StackItem>
-              <code>{`docker pull ${image.repository}@${selectedDigest}`}</code>
-            </StackItem>
-            <StackItem>
-              <code>{`podman pull ${image.repository}@${selectedDigest}`}</code>
-            </StackItem>
-          </Stack>
-        </ModalBody>
-      </Modal>
     </DataListItem>
   );
 }
